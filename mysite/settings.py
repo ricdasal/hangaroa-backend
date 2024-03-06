7b10a1fe5e5a5801511140a0bbc95fe39d93e692
Ricardo <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>
2024-03-06 18:32:56 +0000

    cambios en settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,9 +77,18 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+     #   'ENGINE': 'django.db.backends.sqlite3',
+      #  'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pruebaRicdasal$default',
+        'USER': 'pruebaRicdasal',
+        'PASSWORD': 'hangaroa123',
+        'HOST': 'pruebaRicdasal.mysql.pythonanywhere-services.com',   # Puedes cambiarlo según tu configuración
+        'PORT': '3306',        # Puedes cambiarlo según tu configuración
     }
 }
 
